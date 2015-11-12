312b0471ec9d1846f5ec5aaff183e5f4cba1e972
George Michokostas <michokostas@gmail.com>
2015-11-12 09:17:09 +0100

    add helper method

--- api.rb
+++ api.rb
@@ -5,6 +5,16 @@ DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/world_ci
 
 require "#{Dir.pwd}/models/world_city.rb"
 
+# helpers
+helpers do
+  def jsonize(city)
+    city.to_json(
+      only: [:id, :city, :city_ascii, :country, :iso2, :iso3, :province],
+      methods: [:location, :population]
+    )
+  end
+end
+
 get '/' do
   "Over 7.300 cities from around the world.<br />"\
   "Included all country/province capitals, major cities and towns, and smaller towns in sparsely inhabited regions."
@@ -14,21 +24,14 @@ get '/cities' do
   content_type :json, 'charset' => 'utf-8'
 
   @cities = WorldCity.all
-  @cities.to_json(
-    only: [:id, :city, :city_ascii, :country, :iso2, :iso3, :province],
-    methods: [:location, :population]
-  )
+  jsonize @cities
 end
 
 get '/cities/:id' do
   content_type :json, 'charset' => 'utf-8'
   @city = WorldCity.get(params[:id])
-  @city.to_json(
-    only: [:id, :city, :city_ascii, :country, :iso2, :iso3, :province],
-    methods: [:location, :population]
-  )
+  jsonize @city
 end
-
 
 not_found do
   "<h1>Not Found</h1>"
